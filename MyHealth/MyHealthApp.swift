04c47f2e99b69915c3730bc0d2cb1b591d6c28f7
ogioldat <tomasz.ogiolda00@gmail.com>
2023-06-14 23:02:11 +0200

    Initial Commit

---
+++ MyHealth/MyHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHealthApp.swift
+//  MyHealth
+//
+//  Created by Tomek Ogiołda on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
